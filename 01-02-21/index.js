8d918e7caf348fbf04f01006a9c780be4410a6c9
Jonathan Kim <jonathankim86@gmail.com>
2021-02-01 14:55:00 -0300

    Tarea terminada - forEach y style

--- 01-02-21/index.js
+++ 01-02-21/index.js
@@ -3,7 +3,15 @@ let tareaButton = document.querySelector('button');
 let tareaList = document.querySelector('ul');
 
 tareaButton.onclick = () => {
-    tareaList.innerHTML += `
-        <li>${tareaInput.value}</li>
-    `
+    tareaInput.value != '' ? tareaList.innerHTML += `<li>${tareaInput.value}</li>` : null
+    tareaEnded();
 };
+
+const tareaEnded = () => {
+    let tareaItems = tareaList.querySelectorAll('li');
+    tareaItems.forEach(
+        item => item.addEventListener('click', () => {
+            item.style.textDecoration = 'line-through'
+        })
+    )
+}
