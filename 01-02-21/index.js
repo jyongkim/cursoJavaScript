11e60603281b8b4338efbd5b4cc4777d71d6fffe
Jonathan Kim <jonathankim86@gmail.com>
2021-02-01 14:07:59 -0300

    Lista de tareas

--- 01-02-21/index.js
+++ 01-02-21/index.js
@@ -1,21 +1,3 @@
-var myLink = document.createElement('a');
-var pageBody = document.querySelector('body');
-
-document.write('Jonathan Kim');
-pageBody.appendChild(myLink);
-
-/* innerHTML: edita el contenido de un elemento.*/
-myLink.innerHTML = ' Soy un link';
-
-/* setAttribute('attr', 'valor'): asigna o modifica los atributos y valores de un elemento. */
-myLink.setAttribute('href', '#otra_pagina')
-
-/* Object.assign(el, 'value'): permite asignar múltiples atributos/valores a un elemento.
-                                para aplicarlo de esta manera debemos insertar un objeto como valor. */
-Object.assign(myLink, {
-    href:'#otra_seccion',
-    target:'_self',
-    title:'Soy un link'
-})
-
-pageBody.innerHTML += '<a href="#"> Soy un nuevo link </a>'
+tareaInput = document.querySelector('input');
+tareaButton = document.querySelector('button');
+tareaList = document.querySelector('ul');
