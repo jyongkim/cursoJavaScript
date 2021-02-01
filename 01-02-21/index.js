787d3bb19045754f8cda07a26dc9f8d91ca51ce3
Jonathan Kim <jonathankim86@gmail.com>
2021-02-01 14:24:23 -0300

    Tareas con appendChild

--- 01-02-21/index.js
+++ 01-02-21/index.js
@@ -1,3 +1,9 @@
-tareaInput = document.querySelector('input');
-tareaButton = document.querySelector('button');
-tareaList = document.querySelector('ul');
+let tareaInput = document.querySelector('input');
+let tareaButton = document.querySelector('button');
+let tareaList = document.querySelector('ul');
+
+tareaButton.onclick = () => {
+    let tareaItem = document.createElement('li');
+    tareaItem.innerHTML = tareaInput.value;
+    tareaList.appendChild(tareaItem);
+};
