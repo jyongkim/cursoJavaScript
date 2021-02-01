10974acafc80a2c38b3398096bbc88837b424e80
Jonathan Kim <jonathankim86@gmail.com>
2021-02-01 14:29:39 -0300

    Tareas con innerHTML +=

--- 01-02-21/index.js
+++ 01-02-21/index.js
@@ -3,7 +3,7 @@ let tareaButton = document.querySelector('button');
 let tareaList = document.querySelector('ul');
 
 tareaButton.onclick = () => {
-    let tareaItem = document.createElement('li');
-    tareaItem.innerHTML = tareaInput.value;
-    tareaList.appendChild(tareaItem);
+    tareaList.innerHTML += `
+        <li>${tareaInput.value}</li>
+    `
 };
