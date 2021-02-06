4a57bb26e54ad7b34a7a6b2f3a44b02105f9ba46
Jonathan Kim <jonathankim86@gmail.com>
2021-02-06 11:43:53 -0300

    Buscar duplicados y eliminar

--- 01-02-21/index.js
+++ 01-02-21/index.js
@@ -3,15 +3,56 @@ let tareaButton = document.querySelector('button');
 let tareaList = document.querySelector('ul');
 
 tareaButton.onclick = () => {
-    tareaInput.value != '' ? tareaList.innerHTML += `<li>${tareaInput.value}</li>` : null
-    tareaEnded();
+    tareaInput.value != '' ?
+        buscarDuplicados(tareaInput.value) : null
+        tareaEnded()
+        deleteItem()
 };
 
 const tareaEnded = () => {
+    let tareaChecked = tareaList.querySelectorAll('input[type="checkbox"]')
     let tareaItems = tareaList.querySelectorAll('li');
     tareaItems.forEach(
-        item => item.addEventListener('click', () => {
-            item.style.textDecoration = 'line-through'
+        (item, i) => item.addEventListener('click', () => {
+            tareaChecked[i].checked ?
+                item.style.textDecoration = 'line-through' :
+                item.style.textDecoration =
+                'none'
         })
     )
+}
+
+const buscarDuplicados = (text) => {
+    let duplicateFound
+    let tareaItems = tareaList.querySelectorAll('label');
+    for (i = 0 ; i < tareaItems.length; i++) {
+        if (tareaItems[i].innerHTML == text) {
+            alert('Ya existe la tarea.')
+            duplicateFound = true
+        }
+    }
+    duplicateFound ? null : agregarItem()
+};
+
+const agregarItem = () => {
+    let tareaItems = tareaList.querySelectorAll('li');
+    let item = tareaItems.length + 1;
+    tareaList.innerHTML += `
+        <li>
+            <input type="checkbox" id="item-${item}">
+            <label for="item-${item}">${tareaInput.value}</label>
+            <button class="delete">
+                &times;
+            </button>
+        </li>`
+};
+
+const deleteItem = () => {
+    let delItems = tareaList.querySelectorAll('.delete')
+    let tareaItems = tareaList.querySelectorAll('li')
+    delItems.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            tareaItems[i].remove()
+        })
+    })
 }
