46da05fdfeae810b8ff4a08033ef7461d74fde53
Jonathan Kim <jonathankim86@gmail.com>
2021-02-13 12:06:07 -0300

    Galeria de imágenes 3

--- 01-02-21/galeria.js
+++ 01-02-21/galeria.js
@@ -1,9 +1,16 @@
 /* Crear una galeria de imágenes que tenga dos botones; uno para la imagen anterior y otro para la siguiente.
 El contenedor de la galeria tendrá una clase o un ID para poder identificarlo */
 
+//Individual
 let galeria = document.querySelectorAll('.galeria li');
 let anterior = document.querySelector('.anterior');
 let siguiente = document.querySelector('.siguiente');
+//Grupal
+let galerias = document.querySelectorAll('.galeria');
+let anteriores = document.querySelectorAll('.anterior');
+let siguientes = document.querySelectorAll('.siguiente');
+
+
 
 const cambiarImagen = (direction) => {
     for(i = 0 ; i < galeria.length ; i++){
@@ -28,3 +35,5 @@ const cambiarImagen = (direction) => {
 
 anterior.addEventListener('click', () => cambiarImagen(false))
 siguiente.addEventListener('click', () => cambiarImagen(true))
+
+anteriores.forEach(anterior => anterior.addEventListener('click', () => cambiarImagen(false)))
